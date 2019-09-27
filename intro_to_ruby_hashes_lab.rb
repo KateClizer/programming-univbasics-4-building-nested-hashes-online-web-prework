ed858084054843e916c01e5a15783990ab5789fe
Katelynne Clizer <kateclizer@gmail.com>
2019-09-27 03:00:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,9 @@ def monopoly_with_third_tier
   # complexity.
   
   monopoly = {
+	  :names => {}
 	  :railroads => {
-	    :names => {}
+	    
 	    :rent_in_dollars => { names: "reading_railroad", 
 	     names1: "pennsylvania_railroad", 
 	     names2: "b_and_o_railroad", 
