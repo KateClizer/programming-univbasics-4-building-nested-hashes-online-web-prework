23bdb6f4787ac6ae62ce32ff9790db6b8451c592
Katelynne Clizer <kateclizer@gmail.com>
2019-09-27 03:13:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,8 @@ def monopoly_with_third_tier
   monopoly = {
 	  :railroads => {
 	    :rent_in_dollars => {key1: "1", key2: "2", key3: "3", key4: "4"},
-	    :names => {key1: "1", key2: "2", key3: "3", key4: "4"}
+	    :names => {key1: "1", key2: "2", key3: "3", key4: "4"},
+	    :one_piece_owned => {}
 	  }
   }
 
